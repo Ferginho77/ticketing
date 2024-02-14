d4bf33f77a2050d79ab36756119ca5635e8f13b7
fergie <fergiefakhirusalam@gmail.com>
2024-02-14 20:25:39 +0700

    home page

---
+++ home.php
@@ -0,0 +1,29 @@
+<?php require 'layouts/navbar.html'; ?>
+    <div class="container text-light mt-5 text-center">
+        <h1>Arctic Monkeys Anniversary Concert</h1>
+        <h3 class="text-warning">Get Ready For The Concert</h3>
+    </div>
+        <!-- Counter Time -->
+        <div class="content text-light mt-5">
+            <div class="launch-time">
+                    <div class="">
+                    <p id="days">00</p>
+                    <span>Days</span>
+                    </div>
+                    <div class="">
+                    <p id="hours">00</p>
+                    <span>Hours</span>
+                    </div>
+                    <div class="">
+                    <p id="minutes">00</p>
+                    <span>Minutes</span>
+                    </div>
+                    <div class="">
+                    <p id="seconds">00</p>
+                    <span>Seconds</span>
+                    </div>
+                </div>
+        </div>   
+
+    <a href="#" class="btn btn-outline-warning mt-3">Get Ticket</a>
+    
